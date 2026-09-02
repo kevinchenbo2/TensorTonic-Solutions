cc95f6db1806603f47e65c7d192b9e6ad6bce8fc
Kevin Chen <147973637+kevinchenbo2@users.noreply.github.com>
2026-09-02 11:41:47 -0500

    Solve pytorch-tensor-creation

--- study-plans/pytorch-basics/pytorch-tensor-creation/pytorch-tensor-creation.py
+++ study-plans/pytorch-basics/pytorch-tensor-creation/pytorch-tensor-creation.py
@@ -5,13 +5,12 @@ def create_tensor(method, shape, value=0.0):
     Returns: list
     """
     if method == "zeros":
-        x = torch.zeros(shape)
-    elif method == "ones":
-        x = torch.ones(shape)
-    elif method == "full":
-        x = torch.full(shape, value)
+        return torch.zeros(shape).tolist()
 
-    else: 
-        raise ValueError("not a valid method")
+    if method == "ones":
+        return torch.ones(shape).tolist()
 
-    return x.tolist()
+    if method == "full":
+        return torch.full(shape, value)
+
+    raise ValueError("not a valid method")
